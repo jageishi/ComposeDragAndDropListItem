d7e669e13d183f98b9baae1d7fb90140ecf212a5
Jun Ageishi <j.ageishi@r-n-i.jp>
2022-04-28 19:01:15 +0900

    途中コミット

--- app/src/main/java/org/ageage/composedraganddroplistitem/MainActivity.kt
+++ app/src/main/java/org/ageage/composedraganddroplistitem/MainActivity.kt
@@ -3,12 +3,15 @@ package org.ageage.composedraganddroplistitem
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.gestures.detectDragGesturesAfterLongPress
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListItemInfo
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.Divider
 import androidx.compose.material.Text
 import androidx.compose.runtime.mutableStateOf
@@ -16,6 +19,9 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.input.pointer.consumeAllChanges
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.unit.dp
 import org.ageage.composedraganddroplistitem.ui.theme.ComposeDragAndDropListItemTheme
 
@@ -43,11 +49,36 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeDragAndDropListItemTheme {
+                val lazyListState = rememberLazyListState()
+                val offset = remember { mutableStateOf(0f) }
+                val initialDraggedElement = remember { mutableStateOf<LazyListItemInfo?>(null) }
+                val currentIndexOfDraggedItem = remember { mutableStateOf<Int?>(null) }
                 val users = remember { mutableStateOf(User.dummyList()) }
-                LazyColumn {
+                LazyColumn(
+                    modifier = Modifier.pointerInput(Unit) {
+                        detectDragGesturesAfterLongPress(
+                            onDragStart = { offset ->
+                                lazyListState.layoutInfo.visibleItemsInfo
+                                    .firstOrNull { item -> offset.y.toInt() in item.offset..(item.offset + item.size) }
+                                    ?.also {
+                                        currentIndexOfDraggedItem.value = it.index
+                                        initialDraggedElement.value = it
+                                    }
+                            },
+                            onDragEnd = {},
+                            onDragCancel = {},
+                            onDrag = { change, dragAmount ->
+                                change.consumeAllChanges()
+                            }
+                        )
+                    },
+                    state = lazyListState
+                ) {
                     items(users.value) { user ->
                         Row(
-                            modifier = Modifier.height(100.dp),
+                            modifier = Modifier
+                                .height(100.dp)
+                                .graphicsLayer(translationY = offset.value),
                             verticalAlignment = Alignment.CenterVertically
                         ) {
                             Text(
